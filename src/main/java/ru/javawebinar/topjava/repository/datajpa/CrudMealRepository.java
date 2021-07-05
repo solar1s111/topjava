0e6055ca990bd6c1f564acc3ec41803c62fc1eab
Иван <vania.muratoff@yandex.ru>
2021-07-05 14:49:20 +0700

    HW05 spring data jpa and profiles

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,31 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    List<Meal> getAllByUserId(Sort sort, int userId);
+
+    @Transactional
+    @Modifying
+    int deleteByIdAndUserId(int id, int userId);
+
+    Meal getByIdAndUserId(int id, int userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :start " +
+            "AND m.dateTime < :end ORDER BY m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(Sort sort,
+                                  @Param("start") LocalDateTime startDateTime,
+                                  @Param("end") LocalDateTime endDateTime,
+                                  @Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,43 +1,59 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
+    private static final Sort SORT_DATE_TIME = Sort.by(Sort.Direction.DESC, "dateTime");
 
     private final CrudMealRepository crudRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    private final EntityManager em;
+
+    public DataJpaMealRepository(CrudMealRepository crudRepository, EntityManager em) {
         this.crudRepository = crudRepository;
+        this.em = em;
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+//        meal.setUser(crudRepository.getById(userId).getUser());
+        meal.setUser(em.getReference(User.class, userId));
+        if (meal.isNew()) {
+            return crudRepository.saveAndFlush(meal);
+        } else if (get(meal.id(), userId) == null) {
+            return null;
+        }
+        return crudRepository.saveAndFlush(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.deleteByIdAndUserId(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.getByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAllByUserId(SORT_DATE_TIME, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getBetweenHalfOpen(SORT_DATE_TIME, startDateTime, endDateTime, userId);
     }
 }
